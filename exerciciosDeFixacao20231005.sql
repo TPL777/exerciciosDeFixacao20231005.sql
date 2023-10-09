d1636ef6c126066e26bc0dbf511245b105631def
TPL777 <143000724+TPL777@users.noreply.github.com>
2023-10-09 00:26:06 -0300

    Função para Listar Livros de um Autor Específico

--- exerciciosDeFixacao20231005.sql
+++ exerciciosDeFixacao20231005.sql
@@ -28,3 +28,35 @@ END //
 
 DELIMITER ;
 
+Função para Listar Livros de um Autor Específico:
+
+DELIMITER //
+
+CREATE FUNCTION listar_livros_por_autor(primeiro_nome VARCHAR(255), ultimo_nome VARCHAR(255)) RETURNS TEXT
+BEGIN
+    DECLARE lista TEXT;
+    SET lista = '';
+
+    DECLARE done INT DEFAULT 0;
+    DECLARE author_id INT;
+    DECLARE cur CURSOR FOR SELECT id FROM Autor WHERE primeiro_nome = primeiro_nome AND ultimo_nome = ultimo_nome;
+
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+
+    OPEN cur;
+
+    FETCH cur INTO author_id;
+
+    IF done = 0 THEN
+        SELECT GROUP_CONCAT(Livro.titulo SEPARATOR ', ') INTO lista
+        FROM Livro_Autor
+        JOIN Livro ON Livro_Autor.id_livro = Livro.id
+        WHERE Livro_Autor.id_autor = author_id;
+    END IF;
+
+    CLOSE cur;
+
+    RETURN lista;
+END //
+
+DELIMITER ;
